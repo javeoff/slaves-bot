6d67b0a93bd49109047293d020c8559c3562a500
ciricc <facevk.02@mail.ru>
2021-04-05 23:13:09 +0300

    Свежак

--- src/common/simple_api/simpleApi.tsx
+++ src/common/simple_api/simpleApi.tsx
@@ -5,7 +5,9 @@ import {
   IUserDataResponseDto,
 } from "./types";
 
-const API_ENDPOINT = "https://peostore.mydzin.ru/api";
+const DEV_API_ENDPOINT = "http://127.0.0.1:5656/";
+let API_ENDPOINT = "https://peostore.mydzin.ru/api";
+API_ENDPOINT = DEV_API_ENDPOINT;
 
 class SimpleApi {
   endPoint: string;
